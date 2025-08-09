1702e3c77ecb08d527b553fb99f2a2f0d7a6731c
Sarah D <firelack2000@gmail.com>
2025-08-09 15:32:56 +0200

    Initial commit

---
+++ src/main/java/firelack/portalsmenus/PortalsMenusDataGenerator.java
@@ -0,0 +1,11 @@
+package firelack.portalsmenus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PortalsMenusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
